4318ab726793bf4a7b1ae526940f03fe327436e4
unknown <727724eucs182@skcet.ac.in>
2025-07-18 15:45:20 +0530

    fix: set BrowserRouter basename for GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,10 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/gas-gazer">
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          {}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
